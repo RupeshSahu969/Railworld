2be428f6a8a2fdf839810e4c27f754a6ff65cd67
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2023-07-17 23:47:55 +0530

    add card

--- First_Project/railworld/src/App.js
+++ First_Project/railworld/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Sidbar from './Components/Sidbar';
+import Title from './Pages/Title';
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-     <Sidbar/>
+    <div>
+        {/* <Navbar/>
+     <Sidbar/> */}
+     <Title/>
     </div>
   );
 }
