27ad2ddaf9f2221175714ea8d084d67e0d3c4261
Rupesh sahu <rupeshsahu969@gmail.com>
2023-09-07 00:30:28 +0530

    springsecurity

---
+++ SpringSecurity/springSecuryti/src/main/java/com/example/demo/SpringSecurytiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurytiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurytiApplication.class, args);
+	}
+
+}
