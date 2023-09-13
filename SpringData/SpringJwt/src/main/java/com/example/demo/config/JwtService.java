1f2f6b79b41166459a30792b8c31d194c7418cf3
Rupesh sahu <rupeshsahu969@gmail.com>
2023-09-13 11:14:35 +0530

    generate token complite

--- SpringData/SpringJwt/src/main/java/com/example/demo/config/JwtService.java
+++ SpringData/SpringJwt/src/main/java/com/example/demo/config/JwtService.java
@@ -16,36 +16,43 @@ import io.jsonwebtoken.io.Decoder;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+
 @Component
 public class JwtService {
-
-	private final String SECERT="";
-	
-	public String generateToken(String username) {
-		Map<String, Object> claims= new HashMap();
-		return createToken(username,claims);
-	}
 	
 	
-	private String createToken(String username, Map<String, Object> claims) {
-		// TODO Auto-generated method stub
-		return Jwts
-				.builder()
-				.setClaims(claims)
-				.setSubject(username)
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
-				.signWith(getSignKey(), SignatureAlgorithm.HS256)
-				.compact();
-	}
+	  private final String SECERT = "nmFWroAYa4iKNJVIcNXskEQGVfStrH+m71f1w9i2q7w4seKlMnf9pHHwQbE7pxUC8UXZNnfjXJgtHy04B/SzEcaUfLOyw5ApL/6v319BfsoXtfxMhzgcmZSBNTjdt+Uq1n4C8BAXCT0p7U9bsUz0wvbAkuZn68gVlV3ndJ+AI7QzSIVMe1t6Z34yOSpcP0oZyqqDWWfnJORd3Xocz25S+WfP2mORmg5yxSBjr32rFPFmnxPwUtaJ27mDqoJ477ukWLWGrd9lOceELOVaRXecNvh1ObaWct7TijuqQZIp3XoI28ZZSGrihck2afnz2VdQDxBe89DgClbaxjZf5XvzCw==";
+		
+	    public String generateToken(String username) {
+	    	  Map<String, Object> claims = new HashMap();
+	    	return createToken(username, claims);
+	    }
+	    
+	    
+	   
 
 
-	private Key getSignKey() {
-		// TODO Auto-generated method stub
-		 byte[] keyCodes =    Decoders.BASE64.decode(SECERT);
+		private String createToken(String username, Map<String, Object> claims) {
+			// TODO Auto-generated method stub
+			return Jwts
+					.builder()
+					.setClaims(claims)
+					.setSubject(username)
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+					.signWith(getSignKey(), SignatureAlgorithm.HS256)
+					.compact();
+		}
+
+
+
+
+
+		private Key getSignKey() {
+			// TODO Auto-generated method stub
+			     byte[] keyCodes =    Decoders.BASE64.decode(SECERT);
 			return Keys.hmacShaKeyFor(keyCodes);
-	}
-
+		}
 
 	
 }
