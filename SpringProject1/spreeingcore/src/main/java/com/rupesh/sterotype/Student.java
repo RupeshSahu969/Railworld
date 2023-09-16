b54db5e911b4325bc5050ad119fa28e90dce87c1
Rupesh sahu <rupeshsahu969@gmail.com>
2023-09-16 23:06:25 +0530

    add bean

---
+++ SpringProject1/spreeingcore/src/main/java/com/rupesh/sterotype/Student.java
@@ -0,0 +1,40 @@
+package com.rupesh.sterotype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	@Value("Rupesh")
+	private String studentName;
+	@Value("Raipur")
+	private String city;
+
+	private List<String> address;
+	
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+	
+}
