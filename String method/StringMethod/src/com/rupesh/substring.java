8ec71586efac3828fbf4a043fcf17d4db2b51806
Rupesh sahu <rupeshsahu969@gmail.com>
2023-12-01 10:32:29 +0530

    add

--- String method/StringMethod/src/com/rupesh/substring.java
+++ String method/StringMethod/src/com/rupesh/substring.java
@@ -5,6 +5,14 @@ public class substring {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String s="java is good";
+		String s1="java is good";
+		//System.out.println(s.substring(0, 6));
+		
+		boolean r=s.equals(s1);
+		
+		System.out.println(r);
+		
 	}
 
 }
